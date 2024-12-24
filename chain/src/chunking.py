e2b6a41a6da18d334bb3dbb50147ec9587a5a343
Kobina Brandon <kobinabrandon@proton.me>
2024-12-24 11:17:30 +0000

    Looking at incorporating and comparing multiple chunking strategies, and possibly using a local vector database so I can experiment without limit

--- chain/src/chunking.py
+++ chain/src/chunking.py
@@ -1,5 +1,6 @@
 from loguru import logger
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import 
 
 from src.config import config
 from general.books import Book
@@ -13,7 +14,8 @@ def split_text_into_chunks(books: list[Book]) -> list[str]:
        chunk_size=config.chunk_size,
        chunk_overlap=config.chunk_overlap,
        length_function=config.length_function,
-       add_start_index=config.add_start_index
+       add_start_index=config.add_start_index,
+       separators=["\n\n", "\n", ".", " "]
     )
     
     chunks = splitter.split_text(text=text)
