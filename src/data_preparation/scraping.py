d317582b61a869f23d94bbf20069a7a5b74d27d5
Kobina Brandon <kobinabrandon@proton.me>
2025-03-20 19:22:42 +0000

    Decided to implement "partial scraping" by another means. But the solution is incomplete
    It works by simply scraping the whole page for text, and then returning
    all the text that comes after the last occurence of the provided string.
    
    To ensure that the text we get is as "pure" as possible, I will have to
    ensure that text from the latter parts of the text which isn't required
    will also be eliminated.

--- src/data_preparation/scraping.py
+++ src/data_preparation/scraping.py
@@ -13,18 +13,14 @@ class Scraper:
         response: requests.Response = requests.get(url=self.url)
         if response.status_code == 200:
             soup = BeautifulSoup(markup=response.text, features="html.parser")
+            raw_text: str = soup.text
 
             if not self.partial:
-                return soup.text
+                return raw_text 
 
             elif self.partial and (self.marker != None):
-                start_point = soup.find(name=self.marker)
-
-                if start_point != None:
-                    next_sibling = start_point.find_next()
-                    return next_sibling.get_text(strip=True) 
-                else:
-                    raise Exception("Unable to parse")
+                start_point = raw_text.rfind(self.marker)
+                return raw_text[start_point:]
 
             elif self.partial and (self.marker == None):
                 raise Exception(f"Partial scraping requested for {self.url} without a marker")
